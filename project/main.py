775604e8bb70989c02b9a12c6d7886a6f062f471
bartschliam <bartschliam@gmail.com>
2024-09-30 17:14:55 +0200

    testing

--- project/main.py
+++ project/main.py
@@ -25,7 +25,7 @@ app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USE_SSL'] = False
 mail = Mail(app)
 application = app
-zuerich_tz = pytz.timezone('Europe/Zurich')
+# zuerich_tz = pytz.timezone('Europe/Zurich')
 
 
 def clear_past_bookings():
@@ -83,17 +83,8 @@ def office():
         spot_id = request.form.get('spot_id')
         start_time = request.form.get('start')
         end_time = request.form.get('end')
-        # Convert the incoming time strings to UTC timestamps
-        start_requested_utc = datetime.strptime(start_time, '%Y-%m-%dT%H:%M').replace(tzinfo=pytz.utc)
-        end_requested_utc = datetime.strptime(end_time, '%Y-%m-%dT%H:%M').replace(tzinfo=pytz.utc)
-
-        # Convert the UTC times to Zurich time
-        start_requested_zurich = start_requested_utc.astimezone(zuerich_tz)
-        end_requested_zurich = end_requested_utc.astimezone(zuerich_tz)
-
-        # Convert the Zurich times back to timestamps for storage
-        start_requested = int(start_requested_zurich.timestamp())
-        end_requested = int(end_requested_zurich.timestamp())
+        start_requested = int(datetime.strptime(start_time, '%Y-%m-%dT%H:%M').timestamp())
+        end_requested = int(datetime.strptime(end_time, '%Y-%m-%dT%H:%M').timestamp())
         parking = Parking.query.filter_by(id=spot_id).first()
         bookings = Booking.query.filter_by(parking_id=spot_id).all()
         if end_requested > start_requested:
@@ -138,6 +129,7 @@ def room():
         end_time = request.form.get('end')
 
         start_requested = int(datetime.strptime(start_time, '%Y-%m-%dT%H:%M').timestamp())
+        print(datetime.strptime(start_time, '%Y-%m-%dT%H:%M'), start_requested)
         end_requested = int(datetime.strptime(end_time, '%Y-%m-%dT%H:%M').timestamp())
         permanent = 'permanent' in request.form
         desk = Desk.query.filter_by(id=desk_id).first()
